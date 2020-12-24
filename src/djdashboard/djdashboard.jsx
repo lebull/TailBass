45ab84fcee445c7ec410d356e080cb3c75d93e4f
Lebull <bulldogboy912000@yahoo.com>
2020-12-24 17:46:56 -0600

    Get dj dashboard started

--- src/djdashboard/djdashboard.jsx
+++ src/djdashboard/djdashboard.jsx
@@ -1,14 +1,31 @@
 // import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-
-import { DJList } from '../components/DJList';
+import { Auth } from 'aws-amplify';
+import { useEffect, useState } from 'react';
 
 function DjDashBoard() {
+
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+      const setUserInfo = async () => {
+        const userInfo = await Auth.currentUserInfo();
+        setState({
+          username: userInfo.username,
+          userinfo: userInfo.attributes,
+        })
+      };
+
+      setUserInfo();
+    },
+    []
+  );
+
   return (
-    <div className="App">
+    <>
       <AmplifySignOut />
-      <DJList />
-    </div>
+      <p>I am {state.username}</p>
+    </>
   );
 }
 
